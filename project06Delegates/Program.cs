e9aa6556fc8a5c3ef55be90dd649ada186c3d1f8
IrynaMaroz <maroz.iryna@gmail.com>
2024-06-18 16:21:57 +0300

    project06Delegates added

---
+++ project06Delegates/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace project06Delegates
+{
+    internal static class Program
+    {
+
+        public delegate void NoParameters();
+
+        static void Main()
+        {
+            NoParameters noPrm = new(ShowMessage);
+
+            noPrm();
+
+            Console.ReadLine();
+        }
+
+        private static void ShowMessage()
+        {
+            Console.WriteLine("Have fun!");
+        }
+    }
+}
